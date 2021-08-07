d7a8c5a85e3feb361cefd56b0b7a993a02564d90
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 21:07:06 +0530

    Add files via upload

---
+++ Controllers/BrowseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    [RoutePrefix("api/v1/aurionpro")]
+    public class BrowseController : ApiController
+    {
+        [Route("CompanyInfo")]
+        public IHttpActionResult GetCompanyInfo()
+        {
+            Random r = new Random();
+            int randNo = r.Next(1,10);
+            if(randNo > 5)
+            {
+                return Ok(new { 
+                        Name = "Siddhesh",
+                        Strength = "50",
+                        Address = "Abc"
+                });
+            }
+            return BadRequest("No Company Found");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Json("Get Called");
+        }
+
+        public IHttpActionResult Post()
+        {
+            return Json("Post Called");
+        }
+    }
+}
